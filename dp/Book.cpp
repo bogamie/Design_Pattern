c379f5afc969067222be5bbbbc082644e718c369
bogamie <bogamie02@gmail.com>
2025-11-11 15:17:33 +0900

    fix: fix unload

--- dp/Book.cpp
+++ dp/Book.cpp
@@ -1,7 +1,10 @@
 #include "Book.h"
 
-Book::Book(int i, string t, string a) : id(i), title(t), author(a), isRented(false) {}
+Book::Book(int i, string t, string a) 
+	: id(i), title(t), author(a), isRented(false) {
+}
 void Book::rent() { isRented = true; }
 void Book::giveBack() { isRented = false; }
 bool Book::rented() const { return isRented; }
 string Book::getTitle() const { return title; }
+
--- dp/Book.h
+++ dp/Book.h
@@ -7,10 +7,19 @@ class Book {
     string title;
     string author;
     bool isRented;
+    int stock;
+	int rentedCount;
 public:
     Book(int i, string t, string a);
     void rent();
     void giveBack();
     bool rented() const;
     string getTitle() const;
+
+	int getStock() const;
+    int getAvailable() const;
+	bool canRent() const;
+
+    void markRented();
+	void markReturned();
 };

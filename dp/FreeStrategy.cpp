7d09d681ff5999ccf60bc40b3280528cd632a67c
jnpyo <lissine010816@naver.com>
2025-11-11 18:18:57 +0900

    test

--- dp/FreeStrategy.cpp
+++ dp/FreeStrategy.cpp
@@ -1,5 +1,4 @@
 #include "FeeStrategy.h"
 
 double StandardFee::calculateFee(int days) const { return days * 1000; }
-double PremiumFee::calculateFee(int days) const { return days * 800; }
 double LateFee::calculateFee(int days) const { return days * 1500; }

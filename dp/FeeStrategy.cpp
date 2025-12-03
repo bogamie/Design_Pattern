40210e3c9f79a111865d58c4becebbd6ddc66dc8
bogamie <bogamie02@gmail.com>
2025-12-03 23:20:59 +0900

    Update fee calculation and add new book/user entries

--- dp/FeeStrategy.cpp
+++ dp/FeeStrategy.cpp
@@ -10,5 +10,5 @@ double LateFee::calculateFee(int days, double discountRate) const {
     if (days < 0) days = 0;
     double fee = 1000.0 * days;
     double late = (days > 7) ? (days - 7) * 500.0 : 0.0;
-    return (fee * (1.0 - discountRate)) + late;
+    return (fee + late) * (1.0 - discountRate);
 }
